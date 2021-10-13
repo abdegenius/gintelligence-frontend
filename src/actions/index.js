1f94f5488f6e6970521bba3bc5b447a4d7d1ee32
abdegenius <abijahjohnnie@gmail.com>
2021-10-14 00:46:05 +0100

    ad

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 export const api = {
-  BASE_URL: "http://127.0.0.1:8000/api",
+  BASE_URL: "https://gi.kidify.com.ng/api",
   FSI_KEY: "0KDc0NGPKaK9TJDW23qXKma6NzIKXfl81634140240"
 };
 
 export const connector = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+  baseURL: "https://gi.kidify.com.ng/api",
   timeout: 5000,
   transformResponse: axios.defaults.transformResponse.concat((data) => {
     return data;
